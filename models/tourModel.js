261c7de746841b9330d925bf811241be0d77d1d8
Gmust <4orgood@gmail.com>
2023-02-14 17:04:20 +0100

    Added validator.js lib and simple value validation in the tourModel.js

--- models/tourModel.js
+++ models/tourModel.js
@@ -1,61 +1,99 @@
 const mongoose = require('mongoose');
-
+const validator = require('validator');
 
 const SIMPLE_TOUR_ERR_MSG = 'A tour must have a';
 
 
 const tourSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: [true, `${SIMPLE_TOUR_ERR_MSG} name`],
-    unique: [true, 'Tour with this name is already exist']
+    name: {
+      type: String,
+      required: [true, `${SIMPLE_TOUR_ERR_MSG} name`],
+      unique: [true, 'Tour with this name is already exist'],
+      trim: true,
+      maxLength: [40, `A tour name mast have less or equal then 40 characters`],
+      minLength: [4, `A tour name mast have more or equal then 4 characters`],
+      validate: {
+        validator: function(val) {
+          const value = val.split(' ').join(' ');
+          return validator.isAlpha(value);
+        },
+        message: 'Name must contain only letters'
+      }
+    },
+    price: {
+      type: Number,
+      required: [true, `${SIMPLE_TOUR_ERR_MSG} price`]
+    },
+    duration: {
+      type: Number,
+      required: [true, `${SIMPLE_TOUR_ERR_MSG} duration`]
+    },
+    maxGroupSize: {
+      type: Number,
+      required: [true, `${SIMPLE_TOUR_ERR_MSG} max group sized`]
+    },
+    difficulty: {
+      type: String,
+      required: [true, `${SIMPLE_TOUR_ERR_MSG} difficulty`],
+      enum: {
+        values: ['easy', 'medium', 'difficult'],
+        message: 'Difficulty must be <easy>, <medium> or <difficult>'
+      }
+    },
+    ratingsAverage: {
+      type: Number,
+      default: 0,
+      min: [1, 'A rating must be more or equal than 1'],
+      max: [5, 'A rating must be lower or equal than 5']
+    },
+    ratingQuantity: {
+      type: Number,
+      default: 0
+    },
+    discountPrice: {
+      type: Number,
+      validate: {
+        validator: function(val) {
+          return val < this.price;
+        },
+        message: 'Discount price ({VALUE}) should be below regular price'
+      }
+    },
+    summary: {
+      type: String,
+      trim: true,
+      required: [true, `${SIMPLE_TOUR_ERR_MSG} summary`]
+    },
+    description: {
+      type: String,
+      trim: true
+    },
+    imageCover: {
+      type: String,
+      required: [true, `${SIMPLE_TOUR_ERR_MSG} cover image`]
+    },
+    images: [String],
+    createdAt: {
+      type: Date,
+      default: new Date(),
+      select: false
+    },
+    startDates: [Date]
   },
-  price: {
-    type: Number,
-    required: [true, `${SIMPLE_TOUR_ERR_MSG} price`]
-  },
-  duration: {
-    type: Number,
-    required: [true, `${SIMPLE_TOUR_ERR_MSG} duration`]
-  },
-  maxGroupSize: {
-    type: Number,
-    required: [true, `${SIMPLE_TOUR_ERR_MSG} max group sized`]
-  },
-  difficulty: {
-    type: String,
-    required: [true, `${SIMPLE_TOUR_ERR_MSG} difficulty`]
-  },
-  ratingsAverage: {
-    type: Number,
-    default: 0
-  },
-  ratingQuantity: {
-    type: Number,
-    default: 0
-  },
-  discountPrice: Number,
-  summary: {
-    type: String,
-    trim: true,
-    required: [true, `${SIMPLE_TOUR_ERR_MSG} summary`]
-  },
-  description: {
-    type: String,
-    trim: true
-  },
-  imageCover: {
-    type: String,
-    required: [true, `${SIMPLE_TOUR_ERR_MSG} cover image`]
-  },
-  images: [String],
-  createdAt: {
-    type: Date,
-    default: new Date(),
-    select: false
-  },
-  startDates: [Date]
+  {
+    toJSON: { virtuals: true },
+    toObject: { virtuals: true }
+  }
+);
+
+tourSchema.virtual('durationWeek').get(function() {
+  return this.duration / 7;
 });
+
+/*tourSchema.pre('aggregate', function (next){
+  console.log(this.pipeline())
+  next()
+})*/
 
 module.exports = mongoose.model('Tour', tourSchema);
 
