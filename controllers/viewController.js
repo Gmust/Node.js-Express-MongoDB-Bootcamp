d8f5e599897e06125c8a41588d7a2c667d10351e
Gmust <4orgood@gmail.com>
2023-03-03 17:40:32 +0100

    Created login page  template

--- controllers/viewController.js
+++ controllers/viewController.js
@@ -25,3 +25,10 @@ exports.getTour = catchAsync(async (req, res) => {
     tour
   });
 });
+
+exports.login = (req, res) => {
+
+  res.status(200).render('login', {
+    title: 'Login'
+  });
+};
--- routes/viewRoutes.js
+++ routes/viewRoutes.js
@@ -5,6 +5,6 @@ const viewRouter = express.Router();
 
 viewRouter.get('/', viewController.getOverview);
 viewRouter.get('/tours/:slug', viewController.getTour);
-
+viewRouter.get('/login', viewController.login)
 
 module.exports = viewRouter;
